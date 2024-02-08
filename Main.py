0f892333433eff7439c5da58cbc3be32cd24261e
Teddy Caulton <118858063+teddycaultonhakkoda@users.noreply.github.com>
2024-02-08 14:50:05 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -103,8 +103,8 @@ with tab2:
         average_salary = np.average(queried_table["SALARY"][queried_table["MAPPED_ROLE_CLEAN"] == job])
         print(average_salary)
 
-        st.text(f"According to the model, this employee has a {outcome}% liklihood of churn this year, anything higher than 12.7% should be looked into")
-        st.text(f"The average salary for someone with this role is {average_salary}, take this into consideration")
+    st.text(f"According to the model, this employee has a {outcome}% liklihood of churn this year, anything higher than 12.7% should be looked into")
+    st.text(f"The average salary for someone with this role is {average_salary}, take this into consideration")
 
 
 
